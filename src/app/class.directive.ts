a6f2a57341ed83507ecae79834e3d70ecdde9fe1
Hafez_001 <Hafez_001@DESKTOP-CQG29VN>
2024-01-01 10:29:56 +0330

    add set property

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,4 +1,4 @@
-import { Directive , ElementRef} from '@angular/core';
+import { Directive , ElementRef,HostBinding,HostListener,Input,Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
@@ -6,8 +6,28 @@ import { Directive , ElementRef} from '@angular/core';
 })
 export class ClassDirective {
 
-  constructor(private elementRef : ElementRef) {
-    this.elementRef.nativeElement.style.backgroundColor = 'red';
-  }
+  //@Input() color : string = 'red';
+  // @HostBinding('style.backgroundColor') backgrounColor : string = 'yellow'
+  // constructor(private elementRef : ElementRef,private renderer : Renderer2) {
+  //   //this.elementRef.nativeElement.style.backgroundColor = 'red';
+  //   // this.renderer.setStyle(this.elementRef.nativeElement,'background-color','red')
+  // }
+  // @HostListener('mouseenter') onMouseOver(){
+  //   //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','red')
+  //   this.backgrounColor = "red"
+  // }
+  // @HostListener('mouseleave') onMouseOut(){
+  //   //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','transparent')
+  //   this.backgrounColor = "green"
+  // }
 
+  // constructor(private elementRef : ElementRef){
+  //   this.elementRef.nativeElement.style.backgrounColor = this.color;
+  // }
+  @Input() set backgroundColor(color : string){
+    this.elementRef.nativeElement.style.backgrounColor = color;
+  }
+  constructor(private elementRef : ElementRef){
+    
+  }
 }
